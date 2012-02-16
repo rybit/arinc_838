2dd5c8b52ef161788a49b7a7c12dedf74a8b1e6b
Liron Yahdav <lyahdav@gmail.com>
2012-02-15 21:53:58 -0800

    Cleanup [23773711]

--- src/main/java/edu/cmu/sv/arinc838/ui/UiRunner.java
+++ src/main/java/edu/cmu/sv/arinc838/ui/UiRunner.java
@@ -3,7 +3,6 @@ package edu.cmu.sv.arinc838.ui;
 import java.io.File;
 
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
 import com.arinc.arinc838.SdfFile;
@@ -28,11 +27,16 @@ public class UiRunner {
 		switch (option) {
 		case 1:
 			String fileName = Keyin.inString(" Enter XML File:");
-			if (!new File(fileName).exists()) {
+			File inFile = new File(fileName);
+			if (!inFile.exists()) {
 				System.out.println("File " + fileName + " does not exist.");
 				mainMenu();
 			} else {
-				xmlMenu(fileName);
+				JAXBContext jaxbContext = JAXBContext.newInstance(SdfFile.class);
+				Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+				sdfFile = (SdfFile) jaxbUnmarshaller.unmarshal(inFile);
+
+				xmlMenu();
 			}
 			break;
 		case 2:
@@ -40,14 +44,7 @@ public class UiRunner {
 		}
 	}
 
-	private static void xmlMenu(String inFileName) throws Exception {
-		if (inFileName != null) {
-			JAXBContext jaxbContext = JAXBContext.newInstance(SdfFile.class);
-			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-			File xmlFile = new File(inFileName);
-			sdfFile = (SdfFile) jaxbUnmarshaller.unmarshal(xmlFile);
-		}
-
+	private static void xmlMenu() throws Exception {
 		System.out.println("============================");
 		System.out.println("| Options:");
 		System.out.println("|        1. Generate XML File");
@@ -63,11 +60,11 @@ public class UiRunner {
 			XdfWriter writer = new XdfWriter(sdfFile);
 			writer.write(outFileName);
 			System.out.println("Wrote file to " + outFileName);
-			xmlMenu(null);
+			xmlMenu();
 			break;
 		case 2:
 			System.out.println("Not yet implemented. Select another option.");
-			xmlMenu(null);
+			xmlMenu();
 			break;
 		case 3:
 			mainMenu();
