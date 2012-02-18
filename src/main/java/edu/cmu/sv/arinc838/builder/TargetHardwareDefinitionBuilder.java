8ed7d0af5e5482af247fab79608b7999754e02db
Mike Deats <mikedeats@gmail.com>
2012-02-18 12:49:19 -0700

    Added missing copyright to files. [23773693]

--- src/main/java/edu/cmu/sv/arinc838/builder/TargetHardwareDefinitionBuilder.java
+++ src/main/java/edu/cmu/sv/arinc838/builder/TargetHardwareDefinitionBuilder.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2012 Chris Ellison, Mike Deats, Liron Yahdav, Ryan Neal,
+ * Brandon Sutherlin, Scott Griffin
+ * 
+ * This software is released under the MIT license
+ * (http://www.opensource.org/licenses/mit-license.php)
+ * 
+ * Created on Feb 7, 2012
+ */
 package edu.cmu.sv.arinc838.builder;
 
 import java.util.ArrayList;
