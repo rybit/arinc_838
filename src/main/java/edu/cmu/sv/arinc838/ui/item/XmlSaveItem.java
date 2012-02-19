6af46a053da803a23fafc82702965b3ba48375ca
Mike Deats <mikedeats@gmail.com>
2012-02-18 17:04:08 -0700

    Added copyright headers to new files and fixed compiler warnings [23714795]

--- src/main/java/edu/cmu/sv/arinc838/ui/item/XmlSaveItem.java
+++ src/main/java/edu/cmu/sv/arinc838/ui/item/XmlSaveItem.java
@@ -1,17 +1,19 @@
+/*
+ * Copyright (c) 2012 Chris Ellison, Mike Deats, Liron Yahdav, Ryan Neal,
+ * Brandon Sutherlin, Scott Griffin
+ * 
+ * This software is released under the MIT license
+ * (http://www.opensource.org/licenses/mit-license.php)
+ * 
+ * Created on Feb 18, 2012
+ */
 package edu.cmu.sv.arinc838.ui.item;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.InputStreamReader;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-
-import com.arinc.arinc838.SdfFile;
 
 import edu.cmu.sv.arinc838.builder.SoftwareDefinitionFileBuilder;
 import edu.cmu.sv.arinc838.ui.Menu;
-import edu.cmu.sv.arinc838.ui.XmlFileMenu;
 import edu.cmu.sv.arinc838.xml.XdfWriter;
 
 public class XmlSaveItem extends AbstractMenuItem {	
