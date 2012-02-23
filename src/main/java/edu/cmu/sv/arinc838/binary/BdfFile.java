9399653268126e118ede775decca74b027f0c96a
Brandon Sutherlin <brandon.sutherlin@west.cmu.edu>
2012-02-22 22:06:59 -0700

    Created BDFWrtier Class and test Class.  1 Test failing for write uint32 [23773793]

---
+++ src/main/java/edu/cmu/sv/arinc838/binary/BdfFile.java
@@ -0,0 +1,27 @@
+package edu.cmu.sv.arinc838.binary;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
+public class BdfFile extends RandomAccessFile {
+
+	public static final int UINT32_LENGTH = 4;
+
+	public BdfFile(File raFile) throws FileNotFoundException {
+		super(raFile, "rw");
+		
+	}
+
+	public void writeUint32(long uInt32) {
+		
+		
+	}
+	
+	
+	public static long asUint32(int value){
+		return value & 0xffffffffL;
+		
+	}
+
+}
---
+++ src/test/java/edu/cmu/sv/arinc838/binary/BdfWriterTest.java
@@ -0,0 +1,37 @@
+package edu.cmu.sv.arinc838.binary;
+
+import static org.testng.Assert.*;
+
+import java.io.File;
+
+import org.testng.annotations.Test;
+
+import com.arinc.arinc838.SdfFile;
+
+import edu.cmu.sv.arinc838.xml.XdfWriter;
+
+
+public class BdfWriterTest {
+	
+	@Test
+	public void writeUint32Test() throws Exception {
+		BdfFile f = new BdfFile (File.createTempFile("tmpFile", ".bdf"));
+		
+		//Grab on more than the max value if we get a negative we know we go boom
+		long uInt32 = (long)Integer.MAX_VALUE;
+		uInt32++;
+		
+		f.writeUint32(uInt32);
+		assertEquals(f.length(),BdfFile.UINT32_LENGTH);
+		f.seek(0);
+		
+		long actualUint32 = BdfFile.asUint32(f.readInt());
+		
+		
+		assertEquals(actualUint32, uInt32, "Expected  uInt32 = " + uInt32 + " received --> " + actualUint32);
+		
+		
+		
+	}
+	
+}
