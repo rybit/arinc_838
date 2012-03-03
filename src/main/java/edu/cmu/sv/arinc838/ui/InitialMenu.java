5941fcc9508dd699b42c35d696e487cd42450b93
Scott Griffin <sgriffinusa@gmail.com>
2012-03-03 11:50:19 -0700

    Added read binary menu item [23773793]

--- src/main/java/edu/cmu/sv/arinc838/ui/InitialMenu.java
+++ src/main/java/edu/cmu/sv/arinc838/ui/InitialMenu.java
@@ -7,6 +7,7 @@ import edu.cmu.sv.arinc838.writer.XdfWriter;
 public class InitialMenu implements Menu {
 	private static final MenuItem[] items = new MenuItem[] { 
 		new XmlReadItem ("Read in Xml File"),
+		new BinaryReadItem("Read in Binary File"),
 		new SaveItem ("Save the file as XML", new XdfWriter ()),
 		new SaveItem ("Save the file as Binary", new BdfWriter ()),
 		new ExitItem()
