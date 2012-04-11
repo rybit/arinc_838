f4d289d3e0574ca008954b3075b3b417d0b2834a
Mike Deats <mikedeats@gmail.com>
2012-04-10 22:26:34 -0600

    Added check to validation to allow it to select the correct validation code for STR64k [27137965]

--- src/main/java/edu/cmu/sv/arinc838/validation/SoftwareDefinitionFileValidator.java
+++ src/main/java/edu/cmu/sv/arinc838/validation/SoftwareDefinitionFileValidator.java
@@ -12,6 +12,7 @@ package edu.cmu.sv.arinc838.validation;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.xml.stream.XMLInputFactory;
@@ -32,8 +33,9 @@ public class SoftwareDefinitionFileValidator {
 	}
 
 	/**
-	 * Validates that the header conforms to the spec. This includes the attributes (e.g. schemaLocation)
-	 * the namespace (e.g. xsi, sdf) and the encoding/version (utf-8, 1.0).
+	 * Validates that the header conforms to the spec. This includes the
+	 * attributes (e.g. schemaLocation) the namespace (e.g. xsi, sdf) and the
+	 * encoding/version (utf-8, 1.0).
 	 * 
 	 * @param xmlFile
 	 * @return
@@ -43,23 +45,27 @@ public class SoftwareDefinitionFileValidator {
 		List<Exception> errors = new ArrayList<Exception>();
 
 		try {
-			xsr = XMLInputFactory.newInstance().createXMLStreamReader(new FileInputStream(xmlFile));
+			xsr = XMLInputFactory.newInstance().createXMLStreamReader(
+					new FileInputStream(xmlFile));
 			xsr.nextTag(); // move to the root
 		} catch (Exception e) {
 			errors.add(e);
 			return errors; // can't work if the xsr can't be created
 		}
 
-		if (!DataValidator.XML_ENCODING.equalsIgnoreCase(xsr.getCharacterEncodingScheme())) {
-			Exception e = new IllegalArgumentException("The XML Encoding is wrong." + 
-					"Expected: " + DataValidator.XML_ENCODING + 
-					" found: " + xsr.getCharacterEncodingScheme());
+		if (!DataValidator.XML_ENCODING.equalsIgnoreCase(xsr
+				.getCharacterEncodingScheme())) {
+			Exception e = new IllegalArgumentException(
+					"The XML Encoding is wrong." + "Expected: "
+							+ DataValidator.XML_ENCODING + " found: "
+							+ xsr.getCharacterEncodingScheme());
 			errors.add(e);
 		}
 		if (!DataValidator.XML_VERSION.equalsIgnoreCase(xsr.getVersion())) {
-			Exception e = new IllegalArgumentException("The XML version is wrong." + 
-					"Expected: " + DataValidator.XML_VERSION + 
-					" found: " + xsr.getVersion());
+			Exception e = new IllegalArgumentException(
+					"The XML version is wrong." + "Expected: "
+							+ DataValidator.XML_VERSION + " found: "
+							+ xsr.getVersion());
 			errors.add(e);
 		}
 
@@ -72,7 +78,8 @@ public class SoftwareDefinitionFileValidator {
 		return errors;
 	}
 
-	public List<Exception> validateSdfFile(SoftwareDefinitionFileDao sdfDao, String sourceFile) {
+	public List<Exception> validateSdfFile(SoftwareDefinitionFileDao sdfDao,
+			String sourceFile) {
 		List<Exception> errors = new ArrayList<Exception>();
 
 		try {
@@ -81,36 +88,43 @@ public class SoftwareDefinitionFileValidator {
 			errors.add(e);
 		}
 
-		errors.addAll(validateSoftwareDescription(sdfDao.getSoftwareDescription(), sourceFile));
-		errors.addAll(validateTargetHardwareDefinitions(sdfDao.getTargetHardwareDefinitions()));
+		errors.addAll(validateSoftwareDescription(
+				sdfDao.getSoftwareDescription(), sourceFile));
+		errors.addAll(validateTargetHardwareDefinitions(sdfDao
+				.getTargetHardwareDefinitions(), sourceFile));
 		errors.addAll(validateFileDefinitions(sdfDao.getFileDefinitions()));
-		errors.addAll(validateSdfIntegrityDefinition(sdfDao.getSdfIntegrityDefinition()));
-		errors.addAll(validateLspIntegrityDefinition(sdfDao.getLspIntegrityDefinition()));
+		errors.addAll(validateSdfIntegrityDefinition(sdfDao
+				.getSdfIntegrityDefinition()));
+		errors.addAll(validateLspIntegrityDefinition(sdfDao
+				.getLspIntegrityDefinition()));
 
 		return errors;
 	}
 
-	public List<Exception> validateSoftwareDescription(SoftwareDescriptionDao softwareDesc, String sourceFile) {
+	public List<Exception> validateSoftwareDescription(
+			SoftwareDescriptionDao softwareDesc, String sourceFile) {
 		List<Exception> errors = new ArrayList<Exception>();
 
 		try {
-			dataVal.validateSoftwarePartNumber(softwareDesc.getSoftwarePartnumber());
+			dataVal.validateSoftwarePartNumber(softwareDesc
+					.getSoftwarePartnumber());
 		} catch (IllegalArgumentException e) {
 			errors.add(e);
 		}
-		String partNumberAsFile = softwareDesc.getSoftwarePartnumber().replace("-", "");
+		String partNumberAsFile = softwareDesc.getSoftwarePartnumber().replace(
+				"-", "");
 		String partNumberAsXDF = partNumberAsFile + ".XDF";
 		String partNumberAsBDF = partNumberAsFile + ".BDF";
-		if (!partNumberAsXDF.equals(sourceFile) && !partNumberAsBDF.equals(sourceFile)) {
+		if (!partNumberAsXDF.equals(sourceFile)
+				&& !partNumberAsBDF.equals(sourceFile)) {
 			errors.add(new IllegalArgumentException(
 					"Source file name did not match software part number. File name was '"
 							+ sourceFile + "', expected '" + partNumberAsXDF
 							+ "' or '" + partNumberAsBDF + "'."));
 		}
 
-		// TODO when do we use XML vs. binary validator?
-		errors.addAll(dataVal.validateStr64kXml(softwareDesc
-				.getSoftwareTypeDescription()));
+		errors.addAll(validateStr64k(softwareDesc.getSoftwareTypeDescription(),
+				sourceFile));
 		try {
 			dataVal.validateHexbin32(softwareDesc.getSoftwareTypeId());
 		} catch (IllegalArgumentException e) {
@@ -120,7 +134,18 @@ public class SoftwareDefinitionFileValidator {
 		return errors;
 	}
 
-	public List<Exception> validateTargetHardwareDefinitions(List<TargetHardwareDefinitionDao> thwDefs) {
+	private List<Exception> validateStr64k(String value, String sourceFile) {
+		String extension = sourceFile.split("\\.")[1];
+
+		if (extension.equals("XDF")) {
+			return dataVal.validateStr64kXml(value);
+		} else {
+			return dataVal.validateStr64kBinary(value);
+		}
+	}
+
+	public List<Exception> validateTargetHardwareDefinitions(
+			List<TargetHardwareDefinitionDao> thwDefs, String sourceFile) {
 		List<Exception> errors = new ArrayList<Exception>();
 
 		for (TargetHardwareDefinitionDao thwDef : thwDefs) {
@@ -128,15 +153,15 @@ public class SoftwareDefinitionFileValidator {
 				continue;
 			}
 			for (String position : thwDef.getPositions()) {
-				// TODO when do we use XML vs. binary validator?
-				errors.addAll(dataVal.validateStr64kXml(position));
+				errors.addAll(validateStr64k(position, sourceFile));
 			}
 		}
 
 		return errors;
 	}
 
-	public List<Exception> validateFileDefinitions(List<FileDefinitionDao> fileDefs) {
+	public List<Exception> validateFileDefinitions(
+			List<FileDefinitionDao> fileDefs) {
 		List<Exception> errors = new ArrayList<Exception>();
 
 		try {
@@ -154,11 +179,13 @@ public class SoftwareDefinitionFileValidator {
 		return errors;
 	}
 
-	public List<Exception> validateSdfIntegrityDefinition(IntegrityDefinitionDao sdfInteg) {
+	public List<Exception> validateSdfIntegrityDefinition(
+			IntegrityDefinitionDao sdfInteg) {
 		return validateIntegrityDefinition(sdfInteg);
 	}
 
-	public List<Exception> validateLspIntegrityDefinition(IntegrityDefinitionDao lspInteg) {
+	public List<Exception> validateLspIntegrityDefinition(
+			IntegrityDefinitionDao lspInteg) {
 		return validateIntegrityDefinition(lspInteg);
 	}
 
@@ -173,12 +200,14 @@ public class SoftwareDefinitionFileValidator {
 			errors.add(e);
 		}
 
-		errors.addAll(validateIntegrityDefinition(fileDef.getFileIntegrityDefinition()));
+		errors.addAll(validateIntegrityDefinition(fileDef
+				.getFileIntegrityDefinition()));
 
 		return errors;
 	}
 
-	public List<Exception> validateIntegrityDefinition(IntegrityDefinitionDao integDef) {
+	public List<Exception> validateIntegrityDefinition(
+			IntegrityDefinitionDao integDef) {
 		List<Exception> errors = new ArrayList<Exception>();
 
 		try {
